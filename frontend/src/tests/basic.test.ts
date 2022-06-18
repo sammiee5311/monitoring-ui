1f7262dc272d7b630e7b8afae625eaa31f6e6241
sammiee5311 <sammiee5311@gmail.com>
2022-06-18 22:43:05 +0900

    [Frontend] Fix test error

--- frontend/src/tests/basic.test.ts
+++ frontend/src/tests/basic.test.ts
@@ -1,9 +1,21 @@
 import { mount } from "@vue/test-utils";
+import { setActivePinia, createPinia } from "pinia";
 import { expect, test } from "vitest";
+import { useServerStore } from "../store/server";
+
 import IDontKnowName from "../components/IDontKnowName.vue";
 
+beforeEach(() => {
+  setActivePinia(createPinia());
+});
+
 test("displays message", () => {
+  const server = useServerStore();
+
+  server.initConnectBackend();
+  server.$patch({ message: "Hello World" });
+
   const wrapper = mount(IDontKnowName);
 
-  expect(wrapper.text()).toContain("Hello World");
+  expect(wrapper.text()).toBe("Hello World");
 });
